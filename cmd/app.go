29391fe5cab5305251ed3208961ba5115462398e
cbrom abody <kb.cbrom@gmail.com>
2021-06-05 02:31:30 +0300

    sim flag changed to sim-rw, root-dir flag created

--- cmd/app.go
+++ cmd/app.go
@@ -9,19 +9,27 @@ import (
 
 func NewApp() *cli.App {
 	var simReadWrite int
+	var mediaDirectory string
 	app := &cli.App{
 		Flags: []cli.Flag{
 			&cli.IntFlag{
-				Name:        "sim",
+				Name:        "sim-rw",
 				Value:       runtime.GOMAXPROCS(0),
 				Usage:       "number of simultaneous read and write.",
 				Aliases:     []string{"s"},
 				Destination: &simReadWrite,
 			},
+			&cli.StringFlag{
+				Name:        "root-dir",
+				Value:       "/mnt/",
+				Usage:       "media root directory to perform test on.",
+				Aliases:     []string{"r"},
+				Destination: &mediaDirectory,
+			},
 		},
 		Action: func(c *cli.Context) error {
 			fmt.Println("running test ...")
-			return RunTest(c, simReadWrite)
+			return RunTest(c, simReadWrite, mediaDirectory)
 		},
 	}
 	return app
